37235b70dea825441bd6ddb0080d5c0f89de70bf
Miguel Do Carmo <devmdocarmo@gmail.com>
2020-09-11 21:03:58 -0300

    initial commit

---
+++ app/src/main/java/com/mdocarmo/climetracker/MotherActivity.kt
@@ -0,0 +1,11 @@
+package com.mdocarmo.climetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mother)
+    }
+}
